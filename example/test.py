d87071722f8926e7fec0b0a8df747986826e5603
re1983 <re1983@msn.com>
2024-09-26 19:13:23 -0600

    LAB NEW PC

--- example/test.py
+++ example/test.py
@@ -45,7 +45,7 @@ def set_position(client, aircraft):
         object containing details about craft's position
     """
 
-    ref = [42.37415695190430, -71.01775360107422, 3048.0]
+    ref = [42.37415695190430, -71.01775360107422, 500.0]
     p = pm.enu2geodetic(aircraft.e, aircraft.n, aircraft.u, ref[0], ref[1], ref[2]) #east, north, up
     client.sendPOSI([*p, aircraft.p, aircraft.r, aircraft.h], aircraft.id)
 
@@ -58,6 +58,7 @@ def run_data_generation(client):
     # Set starting position of ownship and intruder
     set_position(client, Aircraft(1, 0, 100, 0, 0, pitch=0, roll=0))
     set_position(client, Aircraft(0, 0, 0, 0, 0, pitch=0, roll=0))
+    client.sendDREFs([dome_offset_heading, dome_offset_pitch], [0, 0])
     client.sendVIEW(85)
     # client.sendVIEW(xpc.ViewType.Tower)
     # dref_camera_type = "sim/cockpit2/camera/camera_type"
@@ -70,18 +71,18 @@ def run_data_generation(client):
 
     # Pause to allow time for user to switch to XPlane window
     # time.sleep(1)
-    # for i in range(250):
-    #     set_position(client, Aircraft(1, 0, 100+i, 0, 135, pitch=0, roll=i))
-    #     time.sleep(0.033)
-    # client.pauseSim(False)
-    for i in range(180):
-        # set_position(client, Aircraft(0, 0, i, 0, 0, pitch=0, roll=0))
-        # client.sendDREFs(camera_DREFs, [-998, -i, i])
-        # client.sendDREFs([dref_heading, dref_pitch, dref_roll], [90.0, 45.0, 30.0])
-        client.sendDREFs([dome_offset_heading, dome_offset_pitch], [0, i])
+    for i in range(100):
+        set_position(client, Aircraft(1, 0, 100+i, 0, 135, pitch=0, roll=i))
         time.sleep(0.033)
-        current_heading = client.getDREFs([dome_offset_heading, dome_offset_pitch]) 
-        print(f"Current Camera heading: {current_heading[0]} degrees")
+    # client.pauseSim(False)
+    for i in range(100):
+        set_position(client, Aircraft(0, 0, i, 0, 0, pitch=0, roll=0))
+        time.sleep(0.033)
+    for i in range(100):
+        client.sendDREFs([dome_offset_heading, dome_offset_pitch], [i, -i])
+        time.sleep(0.033)
+        # current_heading = client.getDREFs([dome_offset_heading, dome_offset_pitch]) 
+        # print(f"Current Camera heading: {current_heading[0]} degrees")
 
 with xpc.XPlaneConnect() as client:
     run_data_generation(client)
